cac1a3148aa3e2845ef0f70433fb93a128125802
DrainChandra <dravinchandra99@gmail.com>
2026-08-17 15:00:36 +0530

    project

---
+++ myyapp/src/components/Drashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from '../assets/ring.jpg';
+
+function Drashboard() {
+  return (
+    <div> <h2>Student details</h2>
+    <h3>Dravin Chandra</h3>
+    <h3>Section - Cse16 </h3></div>
+  )
+}
+
+export default Drashboard
---
+++ myyapp/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import s1 from "../assets/de.jpg"
+
+const Home = () => {
+  return (
+    <div>Home
+        <h2>welcoaa</h2>
+        <img src={s1} alt="loading"  height ={200}/>
+    </div>
+  )
+}
+
+export default Home
